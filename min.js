3ded14c1290b05da26d0419a537df56fba10533e
imran2024bd <imranitbd34@gmail.com>
2024-02-05 01:55:07 +0600

    Find The Cheapest Phone From An Array Of Phone Objects

--- min.js
+++ min.js
@@ -13,4 +13,51 @@ function getMinprice(numbers) {
     return min;
 }
 const cheap = getMinprice(prices);
-console.log(cheap);
+// console.log(cheap);
+
+
+// *****************************************************************
+
+const phones =[
+    {name: 'samsung', price: 20000, camera: '12mp', color: 'black'},
+    {name: 'xaomi', price: 18000, camera: '12mp', color: 'black'},
+    {name: 'Oppo', price: 30000, camera: '12mp', color: 'black'},
+    {name: 'Iphone', price: 100000, camera: '12mp', color: 'black'},
+    {name: 'walton', price: 31000, camera: '12mp', color: 'black'},
+    {name: 'HTC', price: 27000, camera: '12mp', color: 'black'},
+]
+
+function getCheapestPhone(phones) {
+    // console.log(phones);
+    let min = phones[0];
+    for( const phone of phones){
+        // console.log(phone);
+        if(phone.price < min.price){
+            min = phone;
+        }
+    }
+    return min ;
+}
+
+// const cheapestPhone = getCheapestPhone(phones);
+// console.log(cheapestPhone);
+
+
+// ********************************************
+
+// max phone price
+
+function getHighestPhone(phones) {
+    // console.log(phones);
+    let max = phones[0];
+    for( const phone of phones){
+        // console.log(phone);
+        if(phone.price > max.price){
+            max = phone;
+        }
+    }
+    return max ;
+}
+
+const highestestPhone = getHighestPhone(phones);
+console.log(highestestPhone);

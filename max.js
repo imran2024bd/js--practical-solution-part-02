430d0b8d269cd1efbcab3f0dbfd3c90a8bacab2f
imran2024bd <imranitbd34@gmail.com>
2024-02-04 23:54:10 +0600

    max & max 3 practise done

--- max.js
+++ max.js
@@ -9,3 +9,19 @@ else{
 }
 
 // inside a function
+
+function getMax(num1 , num2) {
+    if (num1 > num2) {
+        return num1
+    }
+    else{
+        return num2
+    }
+}
+
+const max1 = getMax(96 , 79);
+console.log(max1);
+const max2 = getMax(56 , 98);
+console.log(max2);
+const ultimateMax = getMax(max1 , max2);
+console.log('max of two is:', ultimateMax);
---
+++ max3.js
@@ -0,0 +1,15 @@
+const jim = 256;
+const tim = 389;
+const kim = 168;
+
+if (jim > tim && jim > kim) {
+    console.log('jim');
+    
+}
+else if (tim > jim && tim > kim) {
+    console.log('tim');
+    
+}
+else{
+    console.log('kim');
+}

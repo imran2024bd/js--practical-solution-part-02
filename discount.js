0bf0c79b440f0d6772234722314070254dc410ef
imran2024bd <imranitbd34@gmail.com>
2024-02-05 16:21:04 +0600

    (Advanced) Multi-Layer Discount Price Calculation

---
+++ discount.js
@@ -0,0 +1,23 @@
+/**
+ * first100 --> 100 tk
+ * 101To200 --> 90  ""
+ * above200 --> 70  ""
+*/
+
+
+function discountedPrice(quantity) {
+    if(quantity <= 100){
+        const total = quantity * 100;
+        return total;
+    }
+    else if( quantity <= 200){
+        const total = quantity *90;
+        return total;
+    }
+    else{
+        const total = quantity * 70;
+        return total;
+    }
+}
+const total = discountedPrice(50);
+console.log(total);
--- layered-discount.js
+++ layered-discount.js
@@ -1,23 +1,35 @@
 /**
- * first100 --> 100 tk
- * 101To200 --> 90  ""
- * above200 --> 70  ""
+ * first100 --> 100
+ * 101To200 --> 90
+ * above200 --> 70
 */
 
-
-function discountedPrice(quantity) {
-    if(quantity <= 100){
-        const total = quantity * 100;
+function layeredDiscountedTotal(quantity) {
+    const first100price = 100;
+    const second100price= 90;
+    const above200price = 70;
+    if( quantity <= 100){
+        const total = quantity * first100price;
         return total;
     }
     else if( quantity <= 200){
-        const total = quantity *90;
+        const total100price = 100 * first100price;
+        const  remainingquantity = 100 - quantity;
+        const remainingtotal = remainingquantity * second100price;
+        const total = total100price + remainingtotal;
         return total;
     }
     else{
-        const total = quantity * 70;
+        const total100price = 100 * first100price;
+        const second100price = 100 * second100price;
+        const remainingquantity = 200 - quantity;
+        const remainingtotal = remainingquantity * 70;
+        const total= total100price + second100price + remainingtotal;
         return total;
+
     }
+    
 }
-const total = discountedPrice(201);
+
+const total = layeredDiscountedTotal(107);
 console.log(total);
